ffc6e3b8c84ca9ba95faa7f9c47936f70cbc274b
Ryo Kasai <ryo.kasai19@gmail.com>
2025-12-08 20:12:52 -0800

    error handling

--- src/improvedMenu/Main.java
+++ src/improvedMenu/Main.java
@@ -16,12 +16,24 @@ public class Main {
 
         System.out.print("Enter day of week (1-7): ");
         int day = in.nextInt();
+        while (1 > day || day > 7) {
+            System.out.print("Input has to be an integer between 1-7 ");
+            day = in.nextInt();
+        }
 
         System.out.print("Enter hour (0-23): ");
         int hour = in.nextInt();
+        while (0 > hour || hour > 23) {
+            System.out.print("Input has to be an integer between 0-23 ");
+            hour = in.nextInt();
+        }
 
         System.out.print("Enter minute (0,10,20,30,40,50): ");
         int minute = in.nextInt();
+        while (minute != 0 || minute != 10 || minute != 20 || minute != 30 || minute != 40 || minute != 50) {
+            System.out.print("Input has to be (0,10,20,30,40,50)");
+            minute = in.nextInt();
+        }
 
         system.runSimulation(day, hour, minute);
         int occupancy = system.getOccupancy(day, hour, minute);
